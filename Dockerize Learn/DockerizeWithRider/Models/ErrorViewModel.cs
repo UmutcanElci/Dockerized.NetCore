6531ff13dbd0f25577bd57fcb9d0fb2f17d99a24
Umutcan Elçi <umutcanelci@yaani.com>
2024-07-10 19:17:45 +0300

    Dockerized .net core project
    This is a test proejct for just learn and try how to dockerize the .net core project.

---
+++ Dockerize Learn/DockerizeWithRider/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DockerizeWithRider.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
