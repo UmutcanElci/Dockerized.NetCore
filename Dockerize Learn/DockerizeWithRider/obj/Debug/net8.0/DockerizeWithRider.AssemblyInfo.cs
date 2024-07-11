ed530ebd5394221fb9dce5d5062192aa59e92e6d
Umutcan Elci <umutcanelci.me@gmail.com>
2024-07-11 09:44:49 +0300

    Dockerized .Net Porject

--- Dockerize Learn/DockerizeWithRider/obj/Debug/net8.0/DockerizeWithRider.AssemblyInfo.cs
+++ Dockerize Learn/DockerizeWithRider/obj/Debug/net8.0/DockerizeWithRider.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DockerizeWithRider")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c6c976b473daae955a7a2aae163ab39bdd8fb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f424606c7331bd0631f42f7457d79be355103ecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DockerizeWithRider")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DockerizeWithRider")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
